7ddea25fcb1c6bb34df9511a7e1315a6e26a889d
gltknky <contact@gltknky.com>
2023-01-17 15:31:22 +0300

    added nodemailer and implemented initial mail send logic

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,46 @@
 const functions = require("firebase-functions");
-
+const nodeMailer = require("nodemailer");
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const handler = (request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+};
+
+exports.helloWorld = functions.https.onRequest(handler);
+
+exports.sendEmail = functions.https.onRequest((request, response) => {
+  try {
+    const transporter = nodeMailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "burakkalayci6718@gmail.com",
+        pass: "yrcgbolmrolkfcwc",
+      },
+    });
+    const options = {
+      from: "burakkalayci6718@gmail.com",
+      to: `${data.email}`,
+      subject: `Message from ${data.name} ${data.surName}`,
+      html: `<p>${data.message}</p>`,
+    };
+    transporter.sendMail(options, function (error, data) {
+      if (error)
+        res.send({
+          success: false,
+          message: error.message,
+        });
+
+      res.send({
+        success: true,
+        message: "we have received your message",
+      });
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
